caae0e165f257c69372a25a97ddc4cb85fd0d00f
erika11111 <104774933+erika11111@users.noreply.github.com>
2023-08-02 13:15:29 +0300

    Added some text & buttons

--- src/components/DisplaySection.jsx
+++ src/components/DisplaySection.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 
 function DisplaySection() {
-  return <div>DisplaySection</div>;
+  return (
+    <div className="display-section wrapper">
+      <h2 className="title">New</h2>
+      <p className="text">Brilliant.</p>
+      <span className="description">
+        A display that's up to 2x brigthter than the sun.
+      </span>
+      {/* preview of a 3d mode & scroll up buttons*/}
+      <button className="button">Try me!</button>
+      <button className="back-button">TOP</button>
+    </div>
+  );
 }
 export default DisplaySection;
