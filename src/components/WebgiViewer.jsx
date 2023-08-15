f7fcdb8dcf6c2a14dd3fd7a24ce1d1423851f6f0
erika11111 <104774933+erika11111@users.noreply.github.com>
2023-08-15 10:57:59 +0200

    Closing preview mode button

--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -32,9 +32,11 @@ const WebgiViewer = forwardRef((props, ref) => {
   const [cameraRef, setCameraRef] = useState(null);
   const [positionRef, setPositionRef] = useState(null);
   const canvasContainerRef = useRef(null);
+  const [previewMode, setPreviewMode] = useState(false);
   //3d animation preview
   useImperativeHandle(ref, () => ({
     triggerPreview() {
+      setPreviewMode(true);
       canvasContainerRef.current.style.pointerEvents = "all";
       props.contentRef.current.style.opacity = "0";
 
@@ -124,9 +126,51 @@ const WebgiViewer = forwardRef((props, ref) => {
   useEffect(() => {
     setupViewer();
   }, []);
+
+  const handleExit = useCallback(() => {
+    canvasContainerRef.current.style.pointerEvents = "none";
+    props.contentRef.current.style.opacity = "1";
+    viewerRef.scene.activeCamera.setCameraOptions({ controlsEnabled: false });
+    setPreviewMode(false);
+
+    gsap.to(positionRef, {
+      x: 1.56,
+      y: 5.0,
+      z: 0.01,
+      scrollTrigger: {
+        trigger: ".display-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        immediateRender: false,
+      },
+      onUpdate: () => {
+        viewerRef.setDirty();
+        cameraRef.positionTargetUpdated(true);
+      },
+    });
+    gsap.to(targetRef, {
+      x: -0.55,
+      y: 0.32,
+      z: 0.0,
+      scrollTrigger: {
+        trigger: ".display-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        immediateRender: false,
+      },
+    });
+  }, [canvasContainerRef, viewerRef, positionRef, cameraRef, targetRef]);
+
   return (
     <div ref={canvasContainerRef} id="webgi-canvas-container">
       <canvas id="webgi-canvas" ref={canvasRef}></canvas>
+      {previewMode && (
+        <button className="button" onClick={handleExit}>
+          Exit
+        </button>
+      )}
     </div>
   );
 });
